b4fa5116242275215e229dd762a3f4e22e06a125
Ramzy842 <54730934+Ramzy842@users.noreply.github.com>
2023-12-13 00:54:27 +0100

    init

---
+++ ex00/Animal.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rchahban <rchahban@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 23:31:24 by rchahban          #+#    #+#             */
+/*   Updated: 2023/12/13 00:38:24 by rchahban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./Animal.hpp"
+
+Animal::Animal()
+{
+	
+}
+Animal::Animal(std::string type)
+{
+	
+}
+Animal::Animal(const Animal& other)
+{
+	
+}
+Animal& Animal::operator=(const Animal& other)
+{
+	
+}
+Animal::~Animal()
+{
+	
+}
